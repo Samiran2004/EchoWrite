9542c8a61232af18b180cf59a808b54badeae23f
Samiran Samanta <samirans170@gmail.com>
2024-06-26 20:12:47 +0530

    Basic setup

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
 
+//Connect with Database...
+mongoose.connect(process.env.DB_URI).then(() => console.log("Database connected...")).catch(() => console.log("Database connection error..."));
+
+//Setup view engine...
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//Middlewares...
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.resolve('./Public')));
+
 app.get('/', (req, res) => {
-    res.json("Hello World...");
+    res.render('homePage');
 });
 
+//Connect with server...
 app.listen(process.env.PORT, (err) => {
     if (err) {
         console.log("Error to connect with server.");
