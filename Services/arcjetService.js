e96f0b82678bdd9fb41ce3a50430af488a4e724d
Samiran Samanta <130337647+Samiran2004@users.noreply.github.com>
2024-12-13 10:16:30 +0530

    modified/Arcjet Service

--- Services/arcjetService.js
+++ Services/arcjetService.js
@@ -3,13 +3,10 @@ import { config } from "dotenv";
 config();
 
 const aj = arcjet({
-    // Get your site key from https://app.arcjet.com and set it as an environment
-    // variable rather than hard coding.
     key: process.env.ARCJET_KEY,
     rules: [
         validateEmail({
-            mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-            // block disposable, invalid, and email addresses with no MX records
+            mode: "LIVE", 
             block: ["DISPOSABLE", "INVALID", "NO_MX_RECORDS"],
         }),
     ],
