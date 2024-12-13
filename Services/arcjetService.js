e54f03faad99a492058fa88ff21a378a223d1066
Samiran Samanta <130337647+Samiran2004@users.noreply.github.com>
2024-12-13 14:10:39 +0530

    Revert "modified/Arcjet Service"
    This reverts commit e96f0b82678bdd9fb41ce3a50430af488a4e724d.

--- Services/arcjetService.js
+++ Services/arcjetService.js
@@ -3,10 +3,13 @@ import { config } from "dotenv";
 config();
 
 const aj = arcjet({
+    // Get your site key from https://app.arcjet.com and set it as an environment
+    // variable rather than hard coding.
     key: process.env.ARCJET_KEY,
     rules: [
         validateEmail({
-            mode: "LIVE", 
+            mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+            // block disposable, invalid, and email addresses with no MX records
             block: ["DISPOSABLE", "INVALID", "NO_MX_RECORDS"],
         }),
     ],
