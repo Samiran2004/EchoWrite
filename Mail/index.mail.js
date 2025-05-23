8fe817ce2eb0bbbad2018a0f250e99535de8a61d
Samiran2004 <samirans170@gmail.com>
2025-05-23 10:49:53 +0530

    update/Mail Template structure for OAuth

--- Mail/index.mail.js
+++ Mail/index.mail.js
@@ -1,8 +1,10 @@
 import ForgotPasswordMailTemplate from "./Template/forgotPassword.mail.template.js";
 import SignUpMailtemplate from "./Template/signup.mail.template.js";
+import SignUp2MailTemplate from "./Template/signup2.mail.js";
 
 const MailTemplates = {
     SignUpMailtemplateContent: SignUpMailtemplate,
+    SignUp2MailtemplateContent: SignUp2MailTemplate,
     ForgotPasswordMailContent: ForgotPasswordMailTemplate
 }
 
